94d47c313dc6395fc4e53e114b32cfe6ec36dccc
AlaminRonya <hprony12@gmail.com>
2022-09-25 22:27:40 +0600

    project create

---
+++ src/main/java/com/example/shapla_city_limited/ShaplaCityLimitedApplication.java
@@ -0,0 +1,13 @@
+package com.example.shapla_city_limited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaplaCityLimitedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaplaCityLimitedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shapla_city_limited/ShaplaCityLimitedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shapla_city_limited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShaplaCityLimitedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
